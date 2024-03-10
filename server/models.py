11d1d6e6dae078f6a4be467b9d662a201174e33f
Boniface <cheruiyotbony@gmail.com>
2024-03-10 22:21:18 +0300

    done

--- server/models.py
+++ server/models.py
@@ -1,8 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
-from sqlalchemy.orm import validates
-from sqlalchemy.ext.associationproxy import association_proxy
-from sqlalchemy_serializer import SerializerMixin
 
 metadata = MetaData(naming_convention={
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
@@ -11,47 +8,46 @@ metadata = MetaData(naming_convention={
 db = SQLAlchemy(metadata=metadata)
 
 
-class Restaurant(db.Model, SerializerMixin):
+class Restaurant(db.Model):
     __tablename__ = 'restaurants'
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
     address = db.Column(db.String)
 
-    # add relationship
-
-    # add serialization rules
+    pizzas = db.relationship('Pizza', secondary='restaurant_pizzas', backref=db.backref('restaurants', lazy=True))
 
     def __repr__(self):
         return f'<Restaurant {self.name}>'
 
 
-class Pizza(db.Model, SerializerMixin):
+class Pizza(db.Model):
     __tablename__ = 'pizzas'
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
     ingredients = db.Column(db.String)
 
-    # add relationship
-
-    # add serialization rules
-
     def __repr__(self):
         return f'<Pizza {self.name}, {self.ingredients}>'
-
-
-class RestaurantPizza(db.Model, SerializerMixin):
+class RestaurantPizza(db.Model):
     __tablename__ = 'restaurant_pizzas'
 
     id = db.Column(db.Integer, primary_key=True)
     price = db.Column(db.Integer, nullable=False)
 
-    # add relationships
+    restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurants.id'), nullable=False)
+    pizza_id = db.Column(db.Integer, db.ForeignKey('pizzas.id'), nullable=False)
 
-    # add serialization rules
+    restaurant = db.relationship('Restaurant', backref=db.backref('restaurant_pizzas', lazy=True))
+    pizza = db.relationship('Pizza', backref=db.backref('restaurant_pizzas', lazy=True))
 
-    # add validation
+    def __init__(self, price, pizza_id, restaurant_id):
+        if not 1 <= price <= 30:
+            raise ValueError("Price must be between 1 and 30")
+        self.price = price
+        self.pizza_id = pizza_id
+        self.restaurant_id = restaurant_id
 
     def __repr__(self):
         return f'<RestaurantPizza ${self.price}>'
